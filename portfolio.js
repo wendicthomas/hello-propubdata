f62e0d502ce8e193c08ff4f78cf007dd217c482f
wendicthomas <wendi.c.thomas@gmail.com>
2016-06-15 00:27:00 -0400

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,60 @@
+$(function () {
+    $('#container').highcharts({
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Blacks are most of the population but few of the wealthy'
+        },
+        xAxis: {
+            categories: ['Black Share of Population', 'Black Over $200K', 'Over $200K']
+        },
+        credits: {
+            enabled: false
+        },
+        series: [{
+            name: 'Population',
+            data: [65]
+        }, {
+            name: 'Making More Than $100K',
+            data: [24]
+        }]
+    });
+
+    $('#container2').highcharts({
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Stacked column chart'
+        },
+        xAxis: {
+            categories: ['Apples', 'Oranges', 'Pears', 'Grapes', 'Bananas']
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Total fruit consumption'
+            }
+        },
+        tooltip: {
+            pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.percentage:.0f}%)<br/>',
+            shared: true
+        },
+        plotOptions: {
+            column: {
+                stacking: 'percent'
+            }
+        },
+        series: [{
+            name: 'John',
+            data: [5, 3, 4, 7, 2]
+        }, {
+            name: 'Jane',
+            data: [2, 2, 3, 2, 1]
+        }, {
+            name: 'Joe',
+            data: [3, 4, 4, 2, 5]
+        }]
+    });
+});
